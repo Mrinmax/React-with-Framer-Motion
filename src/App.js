f71bfbd246df60732277c8f5954079a5b4edf918
Mrinal kanti sardar <mrinalkantisardar007@gmail.com>
2022-08-01 22:27:27 +0530

    First FM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div className="App">
-      <h1>React-Framer-Motion</h1>
+      <Hero />
     </div>
   );
 }
